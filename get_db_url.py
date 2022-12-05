3487d39232d0866da04ea38927af48723105d838
allantestaten <112443674+allantestaten@users.noreply.github.com>
2022-12-05 07:12:43 -0600

    updated doc strings

--- get_db_url.py
+++ get_db_url.py
@@ -2,14 +2,17 @@ from env import host, username, password
 import pandas as pd
 
 def get_db_url(db, user = username, host = host, password = password):
+    '''This function will return the credentials to access the chosen database'''
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
 def make_employees_db():
+    '''this function will read the first 10 lines of the data retrieved from SQL'''
     return pd.read_sql('''SELECT *
 FROM departments
 LIMIT 10''', get_db_url('employees'))
 
 def make_db(db, x):
+    '''this function will run the read sql code to retrieve a database'''
     return pd.read_sql(
         x, get_db_url(db)
     )
